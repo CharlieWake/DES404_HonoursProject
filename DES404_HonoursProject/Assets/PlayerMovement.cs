4ba535424ef5835b646d53c77e979265065d0a5c
CHARLIE WAKE <charlie.wake@hotmail.com>
2025-01-27 14:40:47 +0000

    Started Building Rail System
    Followed Mix & Jam's Star Fox Rail Movement tutorial

---
+++ DES404_HonoursProject/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float xySpeed = 18;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        LocalMove(h, v, xySpeed);
+        ClampPosition();
+    }
+
+    void LocalMove(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+    }
+
+    void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+}
