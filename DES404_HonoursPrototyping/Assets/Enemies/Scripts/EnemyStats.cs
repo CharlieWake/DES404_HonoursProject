ef03f7414990fbd4449c7eae89abe818bd69105b
CharlieWake <127358470+CharlieWake@users.noreply.github.com>
2025-04-07 21:25:08 +0100

    Added Floating Enemy Health Bars
    - Added a FloatingHealthBar script
    - Updated the Enemy Prefab to have a canvas and slider
    - Added the floatingHealthBar script to the slider and pointed it at the enemy's HP and MaxHP values

--- DES404_HonoursPrototyping/Assets/Enemies/Scripts/EnemyStats.cs
+++ DES404_HonoursPrototyping/Assets/Enemies/Scripts/EnemyStats.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 10;
-    private int health = 10;
+    [SerializeField] private float maxHealth = 10;
+    [SerializeField] FloatingHealthBar healthBar;
+    private float health = 10;
     public int actionsPerTurn = 1;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
     }
 
-    public void TakeDamage(int damageAmount)
+    public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
-        
+        healthBar.UpdateHealthBar(health, maxHealth);
         if (health <= 0)
         {
             Death();
